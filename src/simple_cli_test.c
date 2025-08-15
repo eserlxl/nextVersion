ee5914eb6f3bf7feeca5a7032be83141e56e34ad
Eser KUBALI <eserlxl@gmail.com>
2025-08-15 20:12:46 +0300

    Add basic test file

---
+++ src/simple_cli_test.c
@@ -0,0 +1,15 @@
+// Copyright © 2025 Eser KUBALI <lxldev.contact@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This file is part of nextVersion test suite and is licensed under
+// the GNU General Public License v3.0 or later.
+// See the LICENSE file in the project root for details.
+//
+// Test fixture for CLI detection testing
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello, world!\n");
+    return 0;
+}
