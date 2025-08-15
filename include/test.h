6ae87bfafd5844104a1427f14590587fba8f0810
Eser KUBALI <eserlxl@gmail.com>
2025-08-15 20:12:47 +0300

    Add header with prototypes

---
+++ include/test.h
@@ -0,0 +1,16 @@
+// Copyright © 2025 Eser KUBALI <lxldev.contact@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This file is part of nextVersion test suite and is licensed under
+// the GNU General Public License v3.0 or later.
+// See the LICENSE file in the project root for details.
+//
+// Test fixture for API breaking change detection
+
+#ifndef TEST_H
+#define TEST_H
+
+int test_function(int param);
+void another_function(void);
+
+#endif
